92a8a3fe3720f9987913c2f1c29e484ace57e317
arafin-rounok <145318786+arefin008@users.noreply.github.com>
2025-05-20 06:33:06 +0600

    Shapes.py

--- Write a Python program to create a class that represents a shape. Include methods to calculate its area and perimeter. Implement subclasses for different shapes like circle, triangle, and square.py
+++ Write a Python program to create a class that represents a shape. Include methods to calculate its area and perimeter. Implement subclasses for different shapes like circle, triangle, and square.py
@@ -1,3 +1,4 @@
+#Write a Python program to create a class that represents a shape. Include methods to calculate its area and perimeter. Implement subclasses for different shapes like circle, triangle, and square 
 import math
 
 class Shape:
